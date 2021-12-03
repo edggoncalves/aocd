9c4c31562fe752de4fa6b1b9901929c743d8f7a7
Edgar Gonçalves <edgar.gomes.goncalves@zalando.de>
2021-12-03 22:20:01 +0100

    Day 3 part 2

--- day_three.py
+++ day_three.py
@@ -9,11 +9,15 @@ def _config() -> list:
     return instructions
 
 
-def part_one(data):
+def do_stuff(data):
     gamma = ''
     epsilon = ''
-    loops = list(range(len(data[0][2:])))
-    for loop in loops:
+    loop = 0
+    finish = len(data[0][2:])
+    oxygen = {n: [] for n in range(finish)}  # common and 1
+    co2 = {n: [] for n in range(finish)}  # uncommon and 0
+
+    while loop != finish:
         current_bin = []
         for number in data:
             bit = number[2:][loop]
@@ -21,13 +25,26 @@ def part_one(data):
         gamma += mode(current_bin)
         epsilon += Counter(current_bin).most_common()[-1][0]
 
-    return int(gamma, 2) * int(epsilon, 2)
+        compare = Counter(current_bin)
+        oxygen_value = '0' if compare['0'] >= compare['1'] else '1'
+        co2_value = '0' if compare['0'] <= compare['1'] else '1'
+        # for i in data:
+        #     if i[2:].startswith(oxygen_value):
+        #         if oxygen[loop]:
+        #             if len(oxygen[loop]) != finish:
+        #                 oxygen[loop] = i
+        #         else:
+        #             oxygen[loop] = i
+        #     elif len(co2[loop]) != finish:
+        #         co2[loop] = i
+        oxygen[loop] = [i for i in data if i[2:].startswith(oxygen_value) and len(oxygen[loop]) != finish]
+        co2[loop] = [i for i in data if i[2:].startswith(co2_value) and len(oxygen[loop]) != finish]
 
+        loop += 1
 
-def part_two(data):
-    oxygen = ''
-    co2 = ''
-    return data
+    print(oxygen, '\n', co2)
+
+    return int(gamma, 2) * int(epsilon, 2), True
 
 
 def main():
@@ -37,8 +54,7 @@ def main():
     ]
 
     # data = _config()
-    result_one = part_one(data)
-    result_two = part_two(data)
+    result_one, result_two = do_stuff(data)
     print(f'Part one: {result_one}')
     print(f'Part two: {result_two}')
 
