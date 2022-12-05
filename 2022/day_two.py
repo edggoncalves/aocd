a136312e8a10fcae40d1540e7e2290b53815fd01
Edgar Gonçalves <edgar.gomes.goncalves@zalando.de>
2022-12-05 13:47:55 +0100

    Part II day 2.

--- 2022/day_two.py
+++ 2022/day_two.py
@@ -4,35 +4,27 @@
 #     'C': 'Z'
 # }
 
-SCORING = {
-    'AX': 4,
-    'AY': 8,
-    'AZ': 3,
-    'BX': 1,
-    'BY': 5,
-    'BZ': 9,
-    'CX': 7,
-    'CY': 2,
-    'CZ': 6,
-}
+SCORING_1 = {'AX': 4, 'AY': 8, 'AZ': 3, 'BX': 1, 'BY': 5, 'BZ': 9, 'CX': 7, 'CY': 2, 'CZ': 6}
+SCORING_2 = {'AX': 3, 'AY': 4, 'AZ': 8, 'BX': 1, 'BY': 5, 'BZ': 9, 'CX': 2, 'CY': 6, 'CZ': 7}
 
 
-def part_one(data) -> int:
-    score = 0
+def solve(data) -> tuple:
+    score_1 = 0
+    score_2 = 0
     for i in data:
         i = i.replace(' ', '')
-        score += SCORING[f'{i}']
-    return score
+        score_1 += SCORING_1[i]
+        score_2 += SCORING_2[i]
+    return score_1, score_2
 
 
 def main():
     with open('day_two.txt', 'r') as f:
         data = f.read().splitlines()
-    part_1 = part_one(data)
-    # part_2 = part_two(data)
+    part_1, part_2 = solve(data)
 
     print(f'Part One: {part_1}')
-    # print(f'Part Two: {part_2}')
+    print(f'Part Two: {part_2}')
 
 
 if __name__ == '__main__':
